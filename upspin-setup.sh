0b9a9ebd6b60df5e70beb3c64e181b093f32f020
Joe Gregorio <joe@bitworking.org>
2019-02-25 10:30:25 -0500

    no upspin, only gcsfuse!

--- upspin-setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -x
-
-sudo loginctl enable-linger $USER
-mkdir -p ~/up/
-mkdir -p ~/.config/systemd/user/
-cp upspin.service ~/.config/systemd/user/
-systemctl --user enable upspin
-
