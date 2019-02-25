d8ecb952ddd40688e71eb98eb160f7229f8bebec
Joe Gregorio <joe@bitworking.org>
2019-02-25 08:55:27 -0500

    more gcsfuse

--- textfiles-setup.sh
+++ textfiles-setup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -x
 
+go get -u github.com/googlecloudplatform/gcsfuse
+cp $GOPATH/bin/gcsfuse $HOME/bin/
 sudo loginctl enable-linger $USER
 mkdir -p ~/textfiles/
 mkdir -p ~/.config/systemd/user/
 cp textfiles.service ~/.config/systemd/user/
 systemctl --user enable textfiles
-
